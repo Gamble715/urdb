b48d0ecdbb6397700af765c44127c74913eef028
MakerSquare Student <students@themakersquare.com>
2013-09-27 11:03:05 -0500

    Add seed data and routed it to the correct controller view

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,14 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  Movie.destroy_all
+  movies = [{ title: 'Life of Pi', 
+              gif: 'http://31.media.tumblr.com/262aced526fa5faf3a035f68e6221ab0/tumblr_mg7rtkmUP51rkrjw8o1_500.gif', 
+              trailer: '<iframe width="560" height="315" src="//www.youtube.com/embed/j9Hjrs6WQ8M" frameborder="0" allowfullscreen></iframe>', 
+              description: "A young man who survives a disaster at sea is hurtled into an epic journey of adventure and discovery. While cast away, he forms an unexpected connection with another survivor: a fearsome Bengal tiger." 
+  },
+]
+ movies.each do |movie|
+  Movie.create(title: movie[:title], gif: movie[:gif], trailer: movie[:trailer], description: movie[:description])
+end

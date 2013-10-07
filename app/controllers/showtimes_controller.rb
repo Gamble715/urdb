28d761162f14c9968a414a5742e981fb489ac3e3
MakerSquare Student <students@themakersquare.com>
2013-10-07 15:01:36 -0500

    Add showtime create action with hard parameters.

---
+++ app/controllers/showtimes_controller.rb
@@ -0,0 +1,21 @@
+class ShowtimesController < ApplicationController
+  # before_action :set_showtime
+
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @showtime = @movie.showtimes.new(showtime_params)
+
+    if @showtime.save
+      redirect_to @showtime.movie, notice: 'Showtime was successfully created.'
+    else
+      redirect_to @showtime.movie, alert: 'Showtime was not successfully created'
+    end
+  end
+
+  private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def showtime_params
+    params[:showtime].permit(:location, :time)
+  end
+end

d8a915124a7ddf5e870f89bc6ce52b68b52d8d3d
MakerSquare Student <students@themakersquare.com>
2013-10-07 16:39:42 -0500

    Add comments with proper associations to movie model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131007185951) do
+ActiveRecord::Schema.define(version: 20131007204712) do
+
+  create_table "comments", force: true do |t|
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "movie_id"
+  end
 
   create_table "movies", force: true do |t|
     t.string   "title"
